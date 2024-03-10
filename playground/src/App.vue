73d6a1e4cf4c458ec23beee8c655581bf7741b98
viclok <110517011+viclok@users.noreply.github.com>
2024-03-10 22:32:19 +1100

    implemented lists and updating timeblocks

---
+++ playground/src/App.vue
@@ -0,0 +1,49 @@
+<script setup>
+  import { ref } from 'vue'
+  import TodoList from './components/TodoList.vue'
+  
+  const daily = ref([
+  'Hamburger',
+  'Pizza',
+  'Spaghetti',
+  'Tacos',
+  'Teriyaki Chicken',
+  ]);
+  const weekly = ref(['hi']);
+  const mustDo = ref(['yo']);
+  const pd = ref(['wa']);
+  const wantToDo = ref(['sup']);
+
+  const selectedMsg = ref('yo')
+
+  const hours = ['00:00', '00:30', '01:00', '01:30', '02:00']
+
+  function updateItem(event) {
+    event.target.innerHTML = selectedMsg.value
+    console.log(selectedMsg)
+  }
+
+</script>
+
+<template>
+  <TodoList :myList="daily" @select="(msg) => selectedMsg = msg"/>
+  <TodoList :myList="weekly" @select="(msg) => selectedMsg = msg"/>
+  <TodoList :myList="mustDo" @select="(msg) => selectedMsg = msg"/>
+  <TodoList :myList="pd" @select="(msg) => selectedMsg = msg"/>
+  <TodoList :myList="wantToDo" @select="(msg) => selectedMsg = msg"/>
+  <table>
+      <thead>
+        <tr>
+          <th>Time</th>
+          <th>Task</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(hour, index) in hours" :key="index">
+          <td>{{ hour }}</td>
+          <td @click="updateItem">
+          </td>
+        </tr>
+      </tbody>
+    </table>
+</template>
---
+++ playground/src/components/TodoList.vue
@@ -0,0 +1,17 @@
+<script setup>
+  import { ref } from "vue"
+  const props = defineProps({
+  myList: Array
+    })
+    const emit = defineEmits(['select'])
+  
+    function selectItem(item) {
+      emit('select', item)
+    }
+</script>
+
+<template>
+    <ul>
+        <li v-for="item in props.myList" @click="selectItem(item)"> {{ item }}</li>
+    </ul>
+</template>
